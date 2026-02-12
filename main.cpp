6367b05a607b971c0d22eb79c3e5d8b50becb74d
CreepyDarwin <taovuokko@users.noreply.github.com>
2026-02-12 23:21:55 +0200

    Animated player model - Day 2

--- main.cpp
+++ main.cpp
@@ -8,43 +8,32 @@ const std::string GameName("Undefined Runner");
 const int FPS{60};
 const int rec_width{50};
 const int rec_height{80};
-int jumpVel{-80};
+float jumpVel{-600.0f};
+int frame{};
+const float updateTime = 1.0 / 12.0;
+
+// nebula X velocity (pixels/second)
+int NebVel{-500};
 
 struct InputState {
   bool jumpPressed;
 };
 
 struct Physics {
-  const int gravity{10};
+  const float gravity{1000.0f};
 };
 
-InputState ReadInput();
-void UpdatePlayer(int &posY, int &velocity, const InputState &input,
-                  const Physics &physics);
+struct Sprite {
 
-int main(void) {
-  Physics physics;
-
-  int posY{height - rec_height};
-  int velocity{0};
-
-  SetTargetFPS(FPS);
-  InitWindow(width, height, GameName.c_str());
-  SetExitKey(KEY_Q);
-
-  // Gameloop
-  while (!WindowShouldClose()) {
-    InputState input = ReadInput();
-    UpdatePlayer(posY, velocity, input, physics);
-    BeginDrawing();
-    ClearBackground(WHITE);
-
-    DrawRectangle(width / 2, posY, rec_width, rec_height, BLUE);
-
-    EndDrawing();
-  }
-  CloseWindow();
-}
+  Texture2D texture;
+  Texture2D nebula;
+  Rectangle scarfyRec;
+  Rectangle nebRec;
+  Vector2 nebPos;
+  Vector2 scarfyPos;
+  float runningTime;
+  bool onGround;
+};
 
 InputState ReadInput() {
   InputState input{};
@@ -52,20 +41,87 @@ InputState ReadInput() {
   return input;
 }
 
-void UpdatePlayer(int &posY, int &velocity, const InputState &input,
-                  const Physics &physics) {
+Sprite LoadScarfy() {
+  Sprite s{};
+  s.onGround = true;
+  s.texture = LoadTexture("textures/scarfy.png");
+  s.scarfyRec = {0, 0, s.texture.width / 6.0f, (float)s.texture.height};
+  s.scarfyPos = {width / 2.0f, height - s.scarfyRec.height};
+  return s;
+}
 
-  bool onGround = (posY >= height - rec_height);
+Sprite LoadNebula() {
+  Sprite n{};
+  n.nebula = LoadTexture("textures/nebula.png");
+  n.nebRec = {0.0f, 0.0f, n.nebula.width / 8.0f, float(n.nebula.height) / 8};
+  n.nebPos = {width, (float)height - n.nebRec.height};
+  return n;
+}
 
-  if (input.jumpPressed && onGround) {
+void DrawScarfy(const Sprite &s) {
+  DrawTextureRec(s.texture, s.scarfyRec, s.scarfyPos, WHITE);
+}
+
+void DrawNebula(const Sprite &n) {
+  DrawTextureRec(n.nebula, n.nebRec, n.nebPos, WHITE);
+}
+void AnimateScrafy(Sprite &s, float dT) {
+  s.runningTime += dT;
+  if (s.onGround && s.runningTime >= updateTime) {
+    s.runningTime = 0.0f;
+    s.scarfyRec.x = frame * s.scarfyRec.width;
+    frame++;
+    if (frame > 5) {
+      frame = 0;
+    }
+  }
+}
+
+void UpdatePlayer(Sprite &player, float &velocity, const InputState &input,
+                  const Physics &physics, float dT) {
+
+  float groundY = height - player.scarfyRec.height;
+
+  if (input.jumpPressed && player.onGround) {
     velocity = jumpVel;
   }
 
-  velocity += physics.gravity;
-  posY += velocity;
+  velocity += physics.gravity * dT;
+  player.scarfyPos.y += velocity * dT;
 
-  if (posY > height - rec_height) {
-    posY = height - rec_height;
-    velocity = 0;
+  if (player.scarfyPos.y > groundY) {
+    player.scarfyPos.y = groundY;
+    velocity = 0.0f;
   }
+  player.onGround = (player.scarfyPos.y >= groundY);
+}
+
+void UpdateNebula(Sprite &nebula, float dT) { nebula.nebPos.x += NebVel * dT; }
+
+int main(void) {
+  Physics physics;
+  float velocity{0};
+
+  SetTargetFPS(FPS);
+  InitWindow(width, height, GameName.c_str());
+  SetExitKey(KEY_Q);
+  Sprite scarfy = LoadScarfy();
+  Sprite nebula = LoadNebula();
+  // Gameloop
+  while (!WindowShouldClose()) {
+    float dT = GetFrameTime();
+    InputState input = ReadInput();
+    UpdatePlayer(scarfy, velocity, input, physics, dT);
+    UpdateNebula(nebula, dT);
+    BeginDrawing();
+    ClearBackground(WHITE);
+    DrawScarfy(scarfy);
+    DrawNebula(nebula);
+    AnimateScrafy(scarfy, dT);
+
+    EndDrawing();
+  }
+  UnloadTexture(scarfy.texture);
+  UnloadTexture(nebula.nebula);
+  CloseWindow();
 }
