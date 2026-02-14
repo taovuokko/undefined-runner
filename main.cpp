ec496457ce8c2d6d27333f4143c37ba36715222b
CreepyDarwin <taovuokko@users.noreply.github.com>
2026-02-14 17:55:04 +0200

    Refactoring Madness... separated palyer / nebula - Day 3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "raylib.h"
+#include <algorithm>
+#include <ctime>
 #include <string>
+#include <vector>
 
 // Hardcoded values
 const int width{800};
@@ -8,12 +11,8 @@ const std::string GameName("Undefined Runner");
 const int FPS{60};
 const int rec_width{50};
 const int rec_height{80};
-float jumpVel{-600.0f};
-int frame{};
 const float updateTime = 1.0 / 12.0;
-
-// nebula X velocity (pixels/second)
-int NebVel{-500};
+const float nebUpdateTime = 1.0f / 12.0;
 
 struct InputState {
   bool jumpPressed;
@@ -23,16 +22,37 @@ struct Physics {
   const float gravity{1000.0f};
 };
 
-struct Sprite {
-
-  Texture2D texture;
+struct Textures {
+  Texture2D scarfy;
   Texture2D nebula;
-  Rectangle scarfyRec;
+};
+
+struct Player {
+
+  Rectangle rec;
+  Vector2 pos;
+  float runningTime;
+  int frame;
+  float jumpVel;
+  float velocity;
+  bool onGround;
+};
+
+struct Nebula {
+
   Rectangle nebRec;
   Vector2 nebPos;
-  Vector2 scarfyPos;
-  float runningTime;
-  bool onGround;
+  float nebRunningTime;
+  float velocityX;
+  int nebFrame;
+  Color tint;
+};
+
+struct GameState {
+  Player player;
+  std::vector<Nebula> nebulae;
+  float spawnTimer{};
+  const float spawnInterval{2.0f};
 };
 
 InputState ReadInput() {
@@ -40,88 +60,153 @@ InputState ReadInput() {
   input.jumpPressed = IsKeyPressed(KEY_SPACE);
   return input;
 }
+Textures LoadTextures() {
+  Textures t{};
+  t.scarfy = LoadTexture("textures/scarfy.png");
+  t.nebula = LoadTexture("textures/nebula.png");
+  return t;
+}
 
-Sprite LoadScarfy() {
-  Sprite s{};
+void UpdatePlayer(Player &player, const InputState &input,
+                  const Physics &physics, float dT);
+
+void UpdateNebula(Nebula &n, float dT);
+
+Player LoadScarfy(const Texture2D &scaTex) {
+  Player s{};
+  s.jumpVel = -600.0f;
   s.onGround = true;
-  s.texture = LoadTexture("textures/scarfy.png");
-  s.scarfyRec = {0, 0, s.texture.width / 6.0f, (float)s.texture.height};
-  s.scarfyPos = {width / 2.0f, height - s.scarfyRec.height};
+  s.rec = {0, 0, scaTex.width / 6.0f, (float)scaTex.height};
+  s.pos = {width / 2.0f, height - s.rec.height};
   return s;
 }
 
-Sprite LoadNebula() {
-  Sprite n{};
-  n.nebula = LoadTexture("textures/nebula.png");
-  n.nebRec = {0.0f, 0.0f, n.nebula.width / 8.0f, float(n.nebula.height) / 8};
+Nebula LoadNebula(const Texture2D &nebTex) {
+  Nebula n{};
+  n.tint = WHITE;
+  n.velocityX = -500.0f;
+  n.nebRec = {0.0f, 0.0f, nebTex.width / 8.0f, float(nebTex.height) / 8};
   n.nebPos = {width, (float)height - n.nebRec.height};
   return n;
 }
 
-void DrawScarfy(const Sprite &s) {
-  DrawTextureRec(s.texture, s.scarfyRec, s.scarfyPos, WHITE);
+void DrawScarfy(const Player &s, const Texture2D &tex) {
+  DrawTextureRec(tex, s.rec, s.pos, WHITE);
 }
 
-void DrawNebula(const Sprite &n) {
-  DrawTextureRec(n.nebula, n.nebRec, n.nebPos, WHITE);
+void DrawNebula(const Nebula &n, const Texture2D &tex) {
+  DrawTextureRec(tex, n.nebRec, n.nebPos, n.tint);
 }
-void AnimateScrafy(Sprite &s, float dT) {
+void AnimateScrafy(Player &s, float dT) {
+
   s.runningTime += dT;
   if (s.onGround && s.runningTime >= updateTime) {
     s.runningTime = 0.0f;
-    s.scarfyRec.x = frame * s.scarfyRec.width;
-    frame++;
-    if (frame > 5) {
-      frame = 0;
+    s.rec.x = s.frame * s.rec.width;
+    s.frame++;
+    if (s.frame > 5) {
+      s.frame = 0;
     }
   }
 }
 
-void UpdatePlayer(Sprite &player, float &velocity, const InputState &input,
-                  const Physics &physics, float dT) {
+void AnimateNebula(Nebula &n, float dT) {
 
-  float groundY = height - player.scarfyRec.height;
-
-  if (input.jumpPressed && player.onGround) {
-    velocity = jumpVel;
+  n.nebRunningTime += dT;
+  if (n.nebRunningTime >= nebUpdateTime) {
+    n.nebRunningTime = 0.0f;
+    n.nebRec.x = n.nebFrame * n.nebRec.width;
+    n.nebFrame++;
+    if (n.nebFrame > 5) {
+      n.nebFrame = 0;
+    }
   }
-
-  velocity += physics.gravity * dT;
-  player.scarfyPos.y += velocity * dT;
-
-  if (player.scarfyPos.y > groundY) {
-    player.scarfyPos.y = groundY;
-    velocity = 0.0f;
-  }
-  player.onGround = (player.scarfyPos.y >= groundY);
 }
 
-void UpdateNebula(Sprite &nebula, float dT) { nebula.nebPos.x += NebVel * dT; }
+void UpdateGame(GameState &state, const InputState &input,
+                const Physics &physics, float dT, const Texture2D &nebTex) {
+  state.spawnTimer += dT;
+  if (state.spawnTimer >= state.spawnInterval) {
+    Nebula newNebula = LoadNebula(nebTex);
+    newNebula.velocityX = -400.0f - (rand() % 200);
+
+    int colorIndex = rand() % 4;
+
+    if (colorIndex == 0)
+      newNebula.tint = PURPLE;
+    if (colorIndex == 1)
+      newNebula.tint = RED;
+    if (colorIndex == 2)
+      newNebula.tint = WHITE;
+    if (colorIndex == 3)
+      newNebula.tint = BLUE;
+
+    state.nebulae.push_back(newNebula);
+    state.spawnTimer = 0.0f;
+  }
+
+  UpdatePlayer(state.player, input, physics, dT);
+
+  for (auto &n : state.nebulae) {
+    UpdateNebula(n, dT);
+  }
+
+  state.nebulae.erase(std::remove_if(state.nebulae.begin(), state.nebulae.end(),
+                                     [](const Nebula &n) {
+                                       return n.nebPos.x < -n.nebRec.width;
+                                     }),
+                      state.nebulae.end());
+}
+
+void UpdatePlayer(Player &player, const InputState &input,
+                  const Physics &physics, float dT) {
+
+  float groundY = height - player.rec.height;
+
+  if (input.jumpPressed && player.onGround) {
+    player.velocity = player.jumpVel;
+  }
+
+  player.velocity += physics.gravity * dT;
+  player.pos.y += player.velocity * dT;
+
+  if (player.pos.y > groundY) {
+
+    player.pos.y = groundY;
+    player.velocity = 0.0f;
+  }
+  player.onGround = (player.pos.y >= groundY);
+}
+
+void UpdateNebula(Nebula &n, float dT) { n.nebPos.x += n.velocityX * dT; }
 
 int main(void) {
   Physics physics;
-  float velocity{0};
+  GameState state;
+  srand(time((nullptr)));
 
   SetTargetFPS(FPS);
   InitWindow(width, height, GameName.c_str());
   SetExitKey(KEY_Q);
-  Sprite scarfy = LoadScarfy();
-  Sprite nebula = LoadNebula();
+  Textures textures = LoadTextures();
+  state.player = LoadScarfy(textures.scarfy);
   // Gameloop
   while (!WindowShouldClose()) {
     float dT = GetFrameTime();
     InputState input = ReadInput();
-    UpdatePlayer(scarfy, velocity, input, physics, dT);
-    UpdateNebula(nebula, dT);
+    UpdateGame(state, input, physics, dT, textures.nebula);
+
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawScarfy(scarfy);
-    DrawNebula(nebula);
-    AnimateScrafy(scarfy, dT);
-
+    DrawScarfy(state.player, textures.scarfy);
+    for (auto &n : state.nebulae) {
+      DrawNebula(n, textures.nebula);
+      AnimateNebula(n, dT);
+    }
+    AnimateScrafy(state.player, dT);
     EndDrawing();
   }
-  UnloadTexture(scarfy.texture);
-  UnloadTexture(nebula.nebula);
+  UnloadTexture(textures.scarfy);
+  UnloadTexture(textures.nebula);
   CloseWindow();
 }
